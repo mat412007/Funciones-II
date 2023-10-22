68afc76fe168c410e0e3561d7d31557f9ea1b56d
Appaz <matias2007play4@gmail.com>
2023-10-22 17:44:43 -0300

    Avance

---
+++ ejercicio25.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_array(int array[], int n){
+    if(n == 0){
+        return 0;
+    }
+    else{
+        int suma = sum_array(array, n-1) + array[n-1] / 5;
+        return suma;
+    }
+}
+
+int main(int argc, char *argv[]){
+    int array[] = {5, 5, 5, 5, 5};
+    int n = sizeof(array) / sizeof(array[0]);
+    int suma = sum_array(array, n);
+    printf("%d\n", suma);
+    return 0;
+}
