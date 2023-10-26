6c358551cfd4106aed1a7e0e5cbba7933286b07a
Appaz <matias2007play4@gmail.com>
2023-10-26 10:27:47 -0300

    Muerte

--- ejercicio25.c
+++ ejercicio25.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sum_array(int array[], int n){
+double sum_array(int array[], int n){
     if(n == 0){
         return 0;
     }
     else{
-        int suma = sum_array(array, n-1) + array[n-1] / 5;
-        return suma;
+        return ((sum_array(array, n-1)*(n-1)) + array[n-1]) / (double) n;
     }
 }
 
 int main(int argc, char *argv[]){
-    int array[] = {5, 5, 5, 5, 5};
+    int array[] = {6, 6, 6, 6, 6};
     int n = sizeof(array) / sizeof(array[0]);
-    int suma = sum_array(array, n);
-    printf("%d\n", suma);
+    double promedio = sum_array(array, n);
+    printf("%f\n", promedio);
     return 0;
 }
